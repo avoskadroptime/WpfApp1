d0c832c96dcf6250eebe03db28f59e1df1066872
Магомедова Аминат Магомедовна <10190208@college.local>
2022-03-03 17:21:25 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "Hello World!";
+            label1.Cursor = Cursors.Arrow;
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            label1.Content = "";
+            label1.Cursor = Cursors.Help;
+        }
+    }
+}
